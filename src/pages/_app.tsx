68618f9654e34c142ea02f02e80349bcbc094720
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2022-09-06 19:52:59 +0530

    feat:added landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="bg-Background.blue min-h-screen h-full">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 };
 
 export default MyApp;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Background.blue": "#0F111E",
+      },
+      fontFamily: {
+        "dm-serif": "DM Serif Display",
+      },
+    },
   },
   plugins: [],
 };
